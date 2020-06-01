5b1248fcf2c18bad7eac5584f720e432154fb60f
Dominic Tremblay <dominictremblay24@gmail.com>
2020-06-01 12:50:15 -0400

    final demo: added creation of reviews

--- movie_app/app/controllers/movies_controller.rb
+++ movie_app/app/controllers/movies_controller.rb
@@ -22,6 +22,7 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     @reviews = @movie.reviews
+    @review = Review.new
   end
 
 
--- movie_app/app/controllers/reviews_controller.rb
+++ movie_app/app/controllers/reviews_controller.rb
@@ -5,3 +5,39 @@ class ReviewsController < ApplicationController
   def destroy
   end
 end
+class ReviewsController < ApplicationController
+  def create
+
+    
+    @movie = Movie.find(params[:movie_id])
+
+    # create a new review using the reviews relationship
+    @review = @movie.reviews.new(review_params)
+    if @review.save 
+      # redirecting to the same movie show page
+      redirect_to movie_path(@movie)
+    else
+      # provide the path to to show.html.erb from the movies folder
+      # otherwise looking by default in reviews folder
+      render 'movies/show'
+    end
+
+  end
+
+  def destroy
+    movie =  Movie.find(params[:movie_id])
+    review = Review.find(params[:id])
+    if review.destroy
+      redirect_to movie_path(movie), notice: 'review deleted'  
+    else 
+      redirect_to movie_path(movie), notice: 'review cannot be deleted'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
+
+end
